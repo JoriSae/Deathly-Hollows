16851a01c883a7fe05b2886f29ca85a00e8429c5
Skybax12358 <1005809@student.sae.edu.au>
2017-07-16 22:12:56 +1000

    Added the Pathfinding logic
    Zombies now seek out the player, however if there is more than 1 zombie on the map the pathfinding will be greatly delayed. Need to find out a fix for this

---
+++ Assets/Pathfinding/Unit.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public Transform target;
+    float speed = 2;
+    Vector2[] path;
+    int targetIndex;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    }
+
+
+    public void OnPathFound(Vector2[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector2 currentWayPoint = path[0];
+
+        while (true)
+        {
+            if ((Vector2)transform.position == currentWayPoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+
+                currentWayPoint = path[targetIndex];
+            }
+            transform.position = Vector2.MoveTowards(transform.position, currentWayPoint, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], new Vector2(0.2f,0.2f));
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
