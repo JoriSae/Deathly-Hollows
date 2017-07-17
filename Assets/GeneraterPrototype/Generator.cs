ba3d7780718b58d0a92d59d72989668097de6463
Liam <liam_watson2003@hotmail.com>
2017-07-17 17:02:03 +1000

    added prototype resource gen, the resources are tagged as 1 2 or 3

---
+++ Assets/GeneraterPrototype/Generator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Generator : MonoBehaviour {
+
+    public GameObject[] SpawnedItems;
+
+    public int XMAX;
+    public int YMAX;
+    public int ItemsToSpawn;
+
+    // Use this for initialization
+    void Start () {
+        for (int x = 0; x < ItemsToSpawn; x++)
+        {
+
+            int rndnum = Random.Range(0, SpawnedItems.Length);
+            Instantiate(SpawnedItems[rndnum],new Vector2(Random.Range(this.transform.position.x - (XMAX / 2), this.transform.position.x + (XMAX /2)),Random.Range(transform.position.y - (YMAX / 2), transform.position.y + (YMAX / 2))) , transform.rotation);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
