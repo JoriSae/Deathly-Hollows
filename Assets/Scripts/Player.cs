0f8398d9b2b3dc14501de992dedebce5017bfcb0
Liam <liam_watson2003@hotmail.com>
2017-07-20 12:59:24 +1000

    Got Sword Swipe working along with zombie health, now i need to get the sword colliding with the zombies

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Damage;
+    public float AttackCooldown;
+    private float AttackTimer;
+    private bool SwordSwinging = false;
+    public int SlashSpeed;
+    private float slashtimer;
+
+    public GameObject head;
+    public GameObject SwordGO;
+
+	// Use this for initialization
+	void Start () {
+        AttackTimer = AttackCooldown;
+        SwordGO.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        AttackFunction();
+        swordswing();
+    }
+
+    void AttackFunction()
+    {
+        //manages the attack cooldown
+        if (AttackTimer > 0)
+            AttackTimer -= Time.deltaTime;
+
+        //if attack is ready 
+        if (AttackTimer <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                SwordGO.transform.rotation = head.transform.rotation;
+                SwordGO.transform.Rotate(Vector3.forward * 280);
+
+               SwordSwinging = true;   
+                             
+                //if attack is initiated then reset the cooldown
+                AttackTimer = AttackCooldown;
+            }//end attack key
+        }//end attack cooldown
+    }//end attack function
+
+    void swordswing()
+    {
+        if (SwordSwinging == true)
+        {
+            
+            SwordGO.SetActive(true);
+            //line up with forward
+            
+            //slash
+            SwordGO.transform.Rotate(Vector3.forward * Time.deltaTime * SlashSpeed);
+            slashtimer += Time.deltaTime * SlashSpeed;
+            if (slashtimer > 180)
+            {
+                SwordGO.transform.rotation = new Quaternion(0, 0, 0, 0);
+                slashtimer = 0;
+                SwordGO.SetActive(false);
+                SwordSwinging = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    public float Health;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Damage(float _Damage)
+    {
+        Health -= _Damage;
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Zombie Killed");
+        }
+    }
+}
