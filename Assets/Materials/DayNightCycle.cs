65f9fe2746c00e4e4b08fc9a92a7bdcdb39acad4
Gun Tool Unity <1002658@student.sae.edu.au>
2017-08-03 18:02:28 +1000

    Day Night Cycle

---
+++ Assets/Materials/DayNightCycle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    float time;
+
+    public Material material;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        time = Mathf.PingPong(Time.time, 5);
+        time = time / 5;
+        print(time);
+        Shader.SetGlobalFloat("_Time2", time);
+        //material.SetFloat("_Time2", time);
+    }
+}
