8a7a47f523fc141b15d698cd0f013fb351147ccc
Skybax12358 <1005809@student.sae.edu.au>
2017-08-14 23:06:11 +1000

    Equipable GUI & Prevention of crafting unless you have the right items
    placeholder Equipable script now added:
    - randomly assigns the item a rarity and type
    - inspector now changes depending if that equipable is armor or a weapon

---
+++ Assets/Equipable.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipable : MonoBehaviour {
+
+    public EquipmentType equipment;
+    public ArmorKind aKind;
+    public WeaponType weapon;
+    public WeaponKind kind;
+    public Rarity rarity;
+
+    public float damage = 1;
+    public float atkSpeed = 1;
+    public float block = 1;
+    public float blockChance = 1;
+
+
+    void Start()
+    {
+        equipment = GetRandomEnum<EquipmentType>();
+        if (equipment == EquipmentType.Weapon)
+        {
+            weapon = GetRandomEnum<WeaponType>();
+            kind = GetRandomEnum<WeaponKind>();
+        }
+        
+        if (equipment == EquipmentType.Armor)
+        {
+            weapon = GetRandomEnum<WeaponType>();
+            aKind = GetRandomEnum<ArmorKind>();
+        }
+        rarity = GetRandomEnum<Rarity>();
+    }
+
+    static T GetRandomEnum<T>()
+    {
+        System.Array A = System.Enum.GetValues(typeof(T));
+        T V = (T)A.GetValue(UnityEngine.Random.Range(0, A.Length));
+        return V;
+    }
+
+    public enum EquipmentType
+    {
+        Weapon,
+        Armor
+    }
+
+    public enum WeaponType
+    {
+        Melee,
+        Ranged,
+        Magic
+    }
+
+    public enum WeaponKind
+    {
+        Sword,
+        Axe,
+        Bow,
+        Wand,
+        Staff
+    }
+
+    public enum Rarity
+    {
+        Common,
+        UnCommon,
+        Rare,
+        UberRare
+    }
+
+    public enum ArmorKind
+    {
+        Chest,
+        Legs,
+        Head,
+        Arms,
+        Boots
+    }
+
+}
--- Assets/Scripts/CraftItem.cs
+++ Assets/Scripts/CraftItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftItem : MonoBehaviour
 {
@@ -25,7 +26,15 @@ public class CraftItem : MonoBehaviour
 
     private void Update()
     {
-       
+        if (stoneAmount <= StoneRequired && woodAmount <= WoodRequired)
+        {
+            GetComponentInChildren<Button>().interactable = false;
+        }
+
+        if (stoneAmount >= StoneRequired && woodAmount >= WoodRequired)
+        {
+            GetComponentInChildren<Button>().interactable = true;
+        }
     }
 
     public void craftItem()
