aeea721d0ca3444d6830b6a348556c633612bce9
Skybax12358 <1005809@student.sae.edu.au>
2017-08-14 23:06:11 +1000

    Equipable GUI
    placeholder Equipable script now added:
    - randomly assigns the item a rarity and type
    - inspector now changes depending if that equipable is armor or a weapon

---
+++ Assets/EquipableEditor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Equipable)), CanEditMultipleObjects]
+public class EquipableEditor : Editor
+{
+
+    public SerializedProperty
+        equipment,
+        aKind,
+        weapon,
+        kind,
+        rarity,
+        damage,
+        atkSpeed,
+        block,
+        blockChance;
+
+    private void OnEnable()
+    {
+        equipment = serializedObject.FindProperty("equipment");
+        aKind = serializedObject.FindProperty("aKind");
+        weapon = serializedObject.FindProperty("weapon");
+        kind = serializedObject.FindProperty("kind");
+        rarity = serializedObject.FindProperty("rarity");
+        damage = serializedObject.FindProperty("damage");
+        atkSpeed = serializedObject.FindProperty("atkSpeed");
+        block = serializedObject.FindProperty("block");
+        blockChance = serializedObject.FindProperty("blockChance");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(equipment);
+
+        Equipable.EquipmentType et = (Equipable.EquipmentType)equipment.enumValueIndex;
+
+        switch (et)
+        {
+            case Equipable.EquipmentType.Armor:
+                EditorGUILayout.PropertyField(rarity, new GUIContent("rarity"));
+                EditorGUILayout.PropertyField(aKind, new GUIContent("aKind"));
+
+                EditorGUILayout.PropertyField(block, new GUIContent("block"));
+                EditorGUILayout.PropertyField(blockChance, new GUIContent("blockChance"));
+                break;
+
+            case Equipable.EquipmentType.Weapon:
+                EditorGUILayout.PropertyField(rarity, new GUIContent("rarity"));
+                EditorGUILayout.PropertyField(weapon, new GUIContent("weapon"));
+                EditorGUILayout.PropertyField(kind, new GUIContent("kind"));
+
+                EditorGUILayout.PropertyField(damage, new GUIContent("damage"));
+                EditorGUILayout.PropertyField(atkSpeed, new GUIContent("atkSpeed"));
+
+                break;
+                
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
